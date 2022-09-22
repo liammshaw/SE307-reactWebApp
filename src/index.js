ff4846a3dde226ed9e9a3110e3dddac2acedab48
liam <liammshaw@gmail.com>
2022-09-22 14:18:32 -0700

    First commit of my react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDomClient from "react-dom/client";
+import MyApp from './MyApp'
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//create container
+const container = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//create root
+const root = ReactDomClient.createRoot(container);
+
+root.render(<MyApp />)
